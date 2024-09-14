61bbe9094eee961a4eb176160d157f9ec0450ee6
H0COK <bug3bunny@gmail.com>
2024-09-14 21:25:59 +0300

    changed folders

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
-    document.getElementById('ip-address').textContent = data.ip;
-});
